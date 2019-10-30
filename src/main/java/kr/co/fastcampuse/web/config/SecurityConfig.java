e17ed94a5f335671675475e1d5adcdc343c1d499
boojongmin <boojongmin@neofect.com>
2019-10-30 13:59:21 +0900

    [day 1 보충] - Security 기본 설정 추가

---
+++ src/main/java/kr/co/fastcampuse/web/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package kr.co.fastcampuse.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/static/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .antMatchers("/logout").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout().permitAll()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.POST.name()))
+                    .logoutSuccessUrl("/login")
+                    .deleteCookies("JSESSIONID")
+                .and()
+                .csrf().disable(); // logout을 위해 csrf disable 추가.
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
