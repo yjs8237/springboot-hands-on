ef8b737178b4f3e12e175e95f4d5463dff2bdfb4
boojongmin <boojongmin@neofect.com>
2019-10-30 13:39:25 +0900

    [day 1 보충] - validator 추가
    - GET /account/create controller에 model 추가
    - controller에 @Valid
    - create.html th:form, validator message 추가

--- src/main/java/kr/co/fastcampuse/web/account/AccountControler.java
+++ src/main/java/kr/co/fastcampuse/web/account/AccountControler.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -36,13 +38,20 @@ public class AccountControler {
     @GetMapping("/account/create")
     public ModelAndView createGet() {
         ModelAndView mv = new ModelAndView("account/create");
+        mv.addObject("account", new AccountDto());
         return mv;
     }
 
 
     @PostMapping("/account/create")
-    public ModelAndView createPost(AccountDto dto) {
-        accountService.create(dto);
-        return new ModelAndView("redirect:/account/list");
+    public ModelAndView createPost(@ModelAttribute("account") @Valid AccountDto dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            ModelAndView mv = new ModelAndView("account/create");
+            mv.addObject("account", dto);
+            return mv;
+        } else {
+            accountService.create(dto);
+            return new ModelAndView("redirect:/account/list");
+        }
     }
 }
--- src/main/java/kr/co/fastcampuse/web/account/dto/AccountDto.java
+++ src/main/java/kr/co/fastcampuse/web/account/dto/AccountDto.java
@@ -6,12 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountDto {
+    @Size(min=3, max=20, message = "username은 3 ~ 20 글자 범위에서 입력해주세요")
     private String username;
+    @Size(min=5, max=20, message = "password는 5 ~ 20 글자 범위에서 입력해주세요")
     private String password;
 
     public AccountModel toModel() {
